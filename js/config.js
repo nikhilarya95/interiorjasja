e69c602cf340734fd0666f52d31dff4c1b4c6ea4
nikhilarya95 <54001740+nikhilarya95@users.noreply.github.com>
2026-04-29 12:19:53 +0000

    Deploying to gh-pages from @ nikhilarya95/interiorjasja@1f6250f7e53c088619fe1a049f51801711866146 🚀

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    EMAILJS_PUBLIC_KEY: 'gPGiuveeX1mCYFyQr',
-    EMAILJS_SERVICE_ID: 'service_g8r5xdn',
-    EMAILJS_TEMPLATE_ID: 'template_rds2zsn'
+    EMAILJS_PUBLIC_KEY: "gPGiuveeX1mCYFyQr",
+    EMAILJS_SERVICE_ID: "service_g8r5xdn",
+    EMAILJS_TEMPLATE_ID: "template_rds2zsn"
 };
